cd02cbebe0d64f5a67ba1c7d4c7aa71cdd873af4
XoM4oiK <shatiydanik@gmail.com>
2025-03-17 01:42:23 +0300

    Добавьте файлы проекта.

---
+++ StudentCppRevisionTask04Project/taskX.cpp
@@ -0,0 +1,23 @@
+﻿#include "tasks.h"
+#include <string>
+#include <iomanip>
+#include <sstream>
+using namespace std;
+
+string taskX(int number) {
+    if (number < 0 || number >= 86400) {
+        return "error"; 
+    }
+
+    int hours = number / 3600;          
+    int minutes = (number % 3600) / 60; 
+    int seconds = number % 60;          
+
+   
+    ostringstream timeStream;
+    timeStream << hours << ":"
+        << setw(2) << setfill('0') << minutes << ":"
+        << setw(2) << setfill('0') << seconds;
+
+    return timeStream.str();
+}
